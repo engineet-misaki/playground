58d9f38f210ffe6043345eabe0e8184b038d17b5
misaki toyama <toyamax2020@gmail.com>
2023-06-21 21:34:31 +0900

    グラフQKお勉強

--- appollo_sample/app/page.tsx
+++ appollo_sample/app/page.tsx
@@ -5,21 +5,31 @@ import {
   InMemoryCache,
   ApolloProvider,
   useQuery,
+  gql,
 } from "@apollo/client";
-import { GetUsersAndTeamsDocument } from "../graphql/dist/generated-client";
+// import { GetUsersAndTeamsDocument } from "../graphql/dist/generated-client";
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
 });
 
+const GET_TEAM = gql`
+  query getTeams {
+    teams {
+      id
+      name
+    }
+  }
+`;
+
 function UsersAndTeams() {
-  const { loading, error, data } = useQuery(GetUsersAndTeamsDocument);
+  const { loading, error, data } = useQuery(GET_TEAM);
 
   if (loading) return <p>Loading...</p>;
   if (error || !data) return <p>Error</p>;
 
-  const { users, teams } = data;
+  const { teams } = data;
 
   return (
     <>
@@ -30,15 +40,6 @@ function UsersAndTeams() {
         })}
       </ul>
       <h1>User List</h1>
-      <ul>
-        {users.map(({ id, name, teamName }) => {
-          return (
-            <li key={id}>
-              <b>{name}</b> belong {teamName} team
-            </li>
-          );
-        })}
-      </ul>
     </>
   );
 }
