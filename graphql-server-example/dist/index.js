572f621909a373d5be4d70faa55940ea8a14dada
misaki toyama <toyamax2020@gmail.com>
2023-07-06 13:30:35 +0900

    reffect のチュートリアル 3.8まで

--- graphql-server-example/dist/index.js
+++ graphql-server-example/dist/index.js
@@ -1,39 +1,77 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import axios from "axios";
+import { RESTDataSource } from "apollo-datasource-rest";
+class jsonPlaceAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = "https://jsonplaceholder.typicode.com/";
+    }
+    async getUsers() {
+        const data = await this.get("/users");
+        return data;
+    }
+    async getUser(id) {
+        const data = await this.get(`/users/${id}`);
+        return data;
+    }
+    async getPosts() {
+        const data = await this.get("/posts");
+        return data;
+    }
+}
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const typeDefs = `
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  myPosts: [Post]
+}
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+type Post {
+  id: ID!
+  title: String!
+  body: String!
+  userId: ID!
+}
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
+type Query {
+  hello(name: String!): String
+  users: [User]
+  user(id: ID!): User
+  posts: [Post]
+}
 `;
-const books = [
-    {
-        title: "The Awakening",
-        author: "Kate Chopin",
-    },
-    {
-        title: "City of Glass",
-        author: "Paul Auster",
-    },
+const users = [
+    { id: "1", name: "John Doe", email: "john@test.com" },
+    { id: "2", name: "Jane Doe", email: "jane@example.com" },
 ];
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => books,
+        hello: (parent, args) => `Hello ${args.name}`,
+        users: async () => {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+            return response.data;
+        },
+        user: async (parent, args) => {
+            let response = await axios.get(`https://jsonplaceholder.typicode.com/users/${args.id}`);
+            return response.data;
+        },
+        posts: async (_, __, contextValue) => {
+            return contextValue.dataSources.jsonPlaceAPI.getPosts();
+        },
+    },
+    User: {
+        myPosts: async (parent) => {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+            const myPosts = response.data.filter((post) => post.userId == parent.id);
+            return myPosts;
+        },
     },
 };
 // The ApolloServer constructor requires two parameters: your schema
@@ -48,5 +86,12 @@ const server = new ApolloServer({
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async () => {
+        return {
+            dataSources: {
+                jsonPlaceAPI: new jsonPlaceAPI(),
+            },
+        };
+    },
 });
 console.log(`🚀  Server ready at: ${url}`);
